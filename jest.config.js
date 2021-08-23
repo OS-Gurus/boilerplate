02eb3a57db7b40fa41ed16cc9cce388e1de54864
Tim Kinnane <timkinnane@gmail.com>
2021-08-23 16:50:02 +1000

    🌳 Dependency and compilation fixes
    - set minimum node version to 14
    - use appropriate types for node version
    - update dependencies
    - change output directory from dist to out
    - extend node version defaults for tsconfig
    - add tslib and helper config for smaller bundle
    - add demo function and test at src/index

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 const tsPreset = require('ts-jest/jest-preset')
+// ☝️ importing presets allows merging multiple
 
-module.exports = {
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
   roots: ['<rootDir>/src'],
   ...tsPreset
 }
+
+module.exports = config
